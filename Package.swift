081781b7d7d1019d4248fef1834dc3cb195fafef
Sam Ritchie <sam@codesplice.com.au>
2020-02-24 10:10:33 +0800

    Fix dependencies

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.0"))
   ],
   targets: [
-    .target(name: "RxSegue", dependencies: ["RxSwift", "RxCocoa"])
+    .target(name: "RxSegue", dependencies: ["RxSwift"])
   ],
   swiftLanguageVersions: [.v5]
 )
