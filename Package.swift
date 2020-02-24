7d2dec63b333b898c36f1cdb9ee8a06d9fd554cc
Sam Ritchie <sam@codesplice.com.au>
2020-02-24 10:36:00 +0800

    downgrade macOS requirements

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
   name: "RxSegue",
   platforms: [
-    .iOS(.v9), .tvOS(.v9), .macOS(.v10_15)
+    .iOS(.v9), .tvOS(.v9), .macOS(.v10_10)
   ],
   products: [
     .library(name: "RxSegue", targets: ["RxSegue"])
