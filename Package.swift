5fb3cafd0927b43170c03e37b2c3504c6ea0c35c
Sam Ritchie <sam@codesplice.com.au>
2020-02-24 10:26:47 +0800

    Renamed target

--- Package.swift
+++ Package.swift
@@ -4,10 +4,10 @@ import PackageDescription
 let package = Package(
   name: "RxSegue",
   platforms: [
-    .iOS(.v8), .tvOS(.v9)
+    .iOS(.v9), .tvOS(.v9), macOS(.v10_15)
   ],
   products: [
-    .library(name: "RxSegue", targets: ["RxSegue-iOS"])
+    .library(name: "RxSegue", targets: ["RxSegue"])
   ],
   dependencies: [
     .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "5.0.0"))
